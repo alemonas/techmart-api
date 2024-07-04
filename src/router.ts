43e89c52e40cf6588768ef83cd9d03eaaffc6711
Alejandro Monasterios <alemonas@gmail.com>
2024-07-04 08:37:59 -0400

    Add basic middlewares and setup nodemon

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,10 @@ import { Router } from 'express';
 
 const router = Router();
 
-router.get('/product', () => {});
+router.get('/product', (req, res) => {
+  res.status(200);
+  res.json({ message: 'product endpoint updated' });
+});
 router.get('/product/:id', () => {});
 router.put('/product/:id', () => {});
 router.post('/product', () => {});
